83fc71b32cfc0a09e06ed088b0c45eeff6d318e9
MathForge <lucarobertavola@outlook.com>
2026-05-10 18:40:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-function nav(){
+function nav() {
   return `
   <nav>
-    <div class="brand">Num8trix</div>
-    <div class="links">
+    <div class="brand" onclick="location.href='index.html'">Num8trix</div>
+    <div class="menu-toggle" onclick="toggleMenu()">
+      <div></div><div></div><div></div>
+    </div>
+    <div class="links" id="navlinks">
       <a href="index.html">Home</a>
       <a href="matrix.html">Matrix</a>
       <a href="polynomial.html">Polynomial</a>
       <a href="factorization.html">Factor</a>
       <a href="equations.html">Equations</a>
     </div>
-  </nav>
-  `;
+  </nav>`;
+}
+
+function toggleMenu() {
+  document.getElementById("navlinks").classList.toggle("open");
 }
