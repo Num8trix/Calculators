03f8084b63478df938e71a33fdad467cf0e3faf5
MathForge <lucarobertavola@outlook.com>
2026-05-10 17:54:18 +0100

    Add navigation function to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function nav(){
+  return `
+    <header>
+      <div class="brand">MathForge</div>
+      <div class="nav">
+        <a href="index.html">Home</a>
+        <a href="matrix.html">Matrix</a>
+        <a href="polynomial.html">Polynomial</a>
+        <a href="factorization.html">Factorization</a>
+        <a href="equations.html">Equations</a>
+      </div>
+    </header>
+  `;
+}
