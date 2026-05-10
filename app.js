6820bad901fb49fb553c6dccdd1feefb320cbbe6
MathForge <lucarobertavola@outlook.com>
2026-05-10 18:02:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function nav(){
   return `
-    <header>
-      <div class="brand">MathForge</div>
-      <div class="nav">
-        <a href="index.html">Home</a>
-        <a href="matrix.html">Matrix</a>
-        <a href="polynomial.html">Polynomial</a>
-        <a href="factorization.html">Factorization</a>
-        <a href="equations.html">Equations</a>
-      </div>
-    </header>
+  <nav>
+    <div class="brand">Num8trix</div>
+    <div class="links">
+      <a href="index.html">Home</a>
+      <a href="matrix.html">Matrix</a>
+      <a href="polynomial.html">Polynomial</a>
+      <a href="factorization.html">Factor</a>
+      <a href="equations.html">Equations</a>
+    </div>
+  </nav>
   `;
 }
